23d3dd1f190d9ac5d0d11fb9f38f2d4b5e909710
Daniel Pop <dpop100@gmail.com>
2025-02-23 18:27:32 +0200

    added tailwind

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -27,8 +27,14 @@ export default async function HomePage() {
             width={65}
           />
         </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
+        {!user && (
+          <h1 className="text-3xl font-bold underline text-red-500">
+            Welcome to your new project.
+          </h1>
+        )}
+        {user && (
+          <h1 className="text-3xl font-bold underline text-red-500">Welcome back, {user.email}</h1>
+        )}
         <div className="links">
           <a
             className="admin"
